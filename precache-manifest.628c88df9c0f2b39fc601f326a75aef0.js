9f751c1b5cb7f2a18cd3eace6444d2b8d76bacb3
Melih <96746220+RMelih@users.noreply.github.com>
2022-02-23 14:29:18 +0100

    Updates

---
+++ precache-manifest.628c88df9c0f2b39fc601f326a75aef0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613f6f6fc8b15ec90ff6d521a8d6c0f9",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "e5f2aec77137107e26d1",
+    "url": "/Todolist/static/js/2.c475bd67.chunk.js"
+  },
+  {
+    "revision": "eee1afc5ba556617cf19",
+    "url": "/Todolist/static/js/main.15f8d286.chunk.js"
+  },
+  {
+    "revision": "7298e5e5b3509b6c3395",
+    "url": "/Todolist/static/js/runtime-main.82fbdbe9.js"
+  }
+]);
